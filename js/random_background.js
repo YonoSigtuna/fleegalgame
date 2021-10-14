1c2267cf158a47bfb91c06c5c8e8f986cdf3d1e8
YonoSigtuna <3124076548@qq.com>
2021-10-14 12:15:05 +0800

    co

--- js/random_background.js
+++
@@ -1,8 +0,0 @@
-function getRandomIntInclusive(min, max) {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min + 1)) + min; //The maximum is inclusive and the minimum is inclusive 
-}
-var currentBackground = getRandomIntInclusive(1, 1);//How many wallpapers on the right
-document.body.style.background = 'url(./background/'+currentBackground+'.jpg)';
-document.body.style.backgroundSize = 'cover';
